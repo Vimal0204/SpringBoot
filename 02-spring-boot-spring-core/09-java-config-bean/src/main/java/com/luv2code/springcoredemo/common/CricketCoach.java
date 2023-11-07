910fff36c11f3cb9e55cfcf7f31d1b18569cc87c
Vimal0204 <80668288+Vimal0204@users.noreply.github.com>
2023-11-07 19:50:27 +0530

    updated

--- 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
+++ 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -8,17 +8,6 @@ public class CricketCoach implements Coach{
     public CricketCoach(){
         System.out.println("In Constructor: "+getClass().getSimpleName());
     }
-    //define our init method
-    @PostConstruct
-    public void doMyStartupStuff(){
-        System.out.println("In doMyStartupStuff() : "+getClass().getSimpleName());
-    }
-    //define our destroy method
-    @PreDestroy
-    public void doMyCleanupStuff(){
-        System.out.println("In doMyCleanupStuff() : "+getClass().getSimpleName());
-    }
-
     @Override
     public String getDailyWorkout() {
         return "Practice Fast Bowling for 15 minutes.";

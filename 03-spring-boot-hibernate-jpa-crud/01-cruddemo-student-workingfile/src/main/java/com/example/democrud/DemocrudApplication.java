8bb0dfd04fe91dfdc9ac1bb832d3cbd4dd57f0ee
Vimal0204 <80668288+Vimal0204@users.noreply.github.com>
2023-11-08 00:22:33 +0530

    update

---
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student-workingfile/src/main/java/com/example/democrud/DemocrudApplication.java
@@ -0,0 +1,21 @@
+package com.example.democrud;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemocrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocrudApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner commandLineRunner(String args[]){
+		return runner->{
+			System.out.println("hello world");
+		};
+	}
+
+}

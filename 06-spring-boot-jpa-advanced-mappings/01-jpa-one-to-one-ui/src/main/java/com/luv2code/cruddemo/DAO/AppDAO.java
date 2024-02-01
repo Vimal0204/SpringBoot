d39d7924c36b67238a734568d83c6068b1136a71
Vimal0204 <80668288+Vimal0204@users.noreply.github.com>
2024-02-02 02:20:50 +0530

    Bidirectional one to one relationship

--- 06-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-ui/src/main/java/com/luv2code/cruddemo/DAO/AppDAO.java
+++
@@ -1,9 +0,0 @@
-package com.luv2code.cruddemo.DAO;
-
-import com.luv2code.cruddemo.entity.Instructor;
-
-public interface AppDAO {
-    void save(Instructor theInstructor);
-    Instructor findInstructorById(int theId);
-    void deleteinstructorById(int theID);
-}

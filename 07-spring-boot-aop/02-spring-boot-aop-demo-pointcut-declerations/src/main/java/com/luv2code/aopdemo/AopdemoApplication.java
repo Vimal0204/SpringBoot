2143b767c0024e742a02deb4ad528f2e9d991cfc
Vimal0204 <80668288+Vimal0204@users.noreply.github.com>
2024-05-16 17:56:35 +0530

    commiting

---
+++ 07-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declerations/src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
@@ -0,0 +1,39 @@
+package com.luv2code.aopdemo;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AopdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopdemoApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO){
+		return runner->{
+//			System.out.println("Hello world!");
+			demoTheBeforeAccount(theAccountDAO,theMembershipDAO);
+		};
+	}
+
+	private void demoTheBeforeAccount(AccountDAO theAccountDAO,MembershipDAO theMembershipDAO) {
+		// call the business method
+		Account theAccount=new Account();
+		theAccount.setName("Madhu");
+		theAccount.setLevel("Platinum");
+		theAccountDAO.addAccount(theAccount,true );
+		theAccountDAO.doWork();
+		theAccountDAO.setName("foobar");
+		theAccountDAO.setServiceCode("silver");
+		String name=theAccountDAO.getName();
+		String serviceCode=theAccountDAO.getServiceCode();
+		// call the membership business method
+		theMembershipDAO.addSillyMember();
+		theMembershipDAO.goToSleep();
+	}
+}

5ea8ab6063e0ece6d403677188eaaffd987dbfd5
Vimal0204 <80668288+Vimal0204@users.noreply.github.com>
2024-05-17 12:34:53 +0530

    Commit adding spring aop with spring mvc

---
+++ 07-spring-boot-aop/spring-boot-mvc-crud-demo-employees/src/main/java/com/luv2code/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
@@ -0,0 +1,63 @@
+package com.luv2code.springboot.thymeleafdemo.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+@Aspect
+@Component
+public class DemoLoggingAspect {
+    // setup the logger
+    private Logger myLogger=Logger.getLogger(getClass().getName());
+
+    //setup pointcut declarations
+    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.controller.*.*(..))")
+    private void forControllerPackage(){}
+    //do the same for service and dao
+    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.service.*.*(..))")
+    private void forServicePackage(){}
+
+    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.dao.*.*(..))")
+    private void forDaoPackage(){}
+
+    @Pointcut("forControllerPackage()||forServicePackage()||forDaoPackage()")
+    private void forAppFlow(){}
+    // add @Before advice
+    @Before("forAppFlow()")
+    public void before(JoinPoint theJoinPoint){
+        //display method we are calling
+        String theMehod=theJoinPoint.getSignature().toShortString();
+        myLogger.info("=====> in @Before: calling method: "+theMehod);
+
+        //display the arguments to the method
+
+        //get the arguments
+        Object[]args=theJoinPoint.getArgs();
+        //loop through and display args
+        for (Object tempArg: args) {
+            myLogger.info("=====> argument: "+tempArg);
+        }
+
+    }
+
+    // add @AfterReturningadvice
+    @AfterReturning(
+            pointcut = "forAppFlow()",
+            returning = "theResult"
+    )
+    public void afterReturning(JoinPoint theJoinPoint,Object theResult){
+        //display method we are returning from
+        String theMehod=theJoinPoint.getSignature().toShortString();
+        myLogger.info("=====> in @AfterReturning: from method: "+theMehod);
+
+        //display data returned
+        myLogger.info("=====>> result: "+theResult);
+
+    }
+
+}
